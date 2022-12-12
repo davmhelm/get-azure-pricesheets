f75ac8589134a51a2c099ad2f4ef203861e38c93
davmhelm <dahelm@microsoft.com>
2022-12-12 10:22:10 -0600

    add retail pricesheet retrieval script

---
+++ get-retail-pricesheets.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ $(echo $@ | grep -F -e '-h' -e '--help') ]]; then
+	echo -e "This command retrieves the currently available azure retail price sheets at the time of execution."
+    echo -e ""
+    echo -e "usage: $0 optional-output-file-name-prefix"
+	echo -e "example: $0 mysheet"
+    echo -e ""
+    echo -e "Price sheets are downloaded in chunks from the public retail price API, and saved in numbered JSON files."
+    echo -e "The file name format is: prefix_timestamp_####.json"
+    echo -e "The default prefix is 'retailpricesheet' but you can specify one as an optional argument."
+    echo -e "Timestamp is represented in yyyymmddHHMMSSz format (for example, $(date +"%Y%m%d%H%M%S%z"))"
+    echo -e ""
+else
+	i=0
+    printf -v index "%04d" $i
+    nextRestCall="https://prices.azure.com/api/retail/prices?api-version=2021-10-01-preview"
+
+    prefix=retailpricesheet
+    timestamp=$(date +"%Y%m%d%H%M%S%z")
+    if [ ! -z "$1" ]; then
+        prefix=$1
+    fi
+    outFile=${prefix}_${timestamp}_${index}.json
+
+	echo -e "Getting price sheet $outFile..."
+    echo -e "curl --no-progress-meter --get "$nextRestCall" -o $outFile"
+    
+	nextRestCall=$( cat $outFile | jq -r '.NextPageLink // ""' )
+    
+    while [ ! -z "$nextRestCall" ]; do
+    	((i++))
+        printf -v index "%04d" $i
+        outFile=${prefix}_${timestamp}_${index}.json
+        echo -e "Getting price sheet $outFile..."
+    	curl --no-progress-meter --get "$nextRestCall" -o $outFile
+    	nextRestCall=$( cat $outFile | jq -r '.NextPageLink // ""' )
+    done
+fi
